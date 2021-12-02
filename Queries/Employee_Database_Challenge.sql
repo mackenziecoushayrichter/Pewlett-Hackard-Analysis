533dd90844dc935bdbee9e5e604ed01b9294b486
mackenziecoushay <mackenziecoushay@Mackenzies-Air.domain>
2021-12-01 20:58:11 -0800

    saving changes

---
+++ Queries/Employee_Database_Challenge.sql
@@ -0,0 +1,28 @@
+--Challenge
+
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	ti.title,
+	ti.from_date,
+	ti.to_date
+INTO retirement_titles
+FROM employees AS e
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
+ORDER BY e.emp_no;
+
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM retirement_titles AS rt
+ORDER BY emp_no ASC, to_date DESC;
+
+SELECT COUNT(first_name),title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(first_name) DESC;
